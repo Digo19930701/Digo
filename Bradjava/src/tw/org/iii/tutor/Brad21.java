e933c679ebe86f14e7a0276d778135a301e7735a
Digo19930701 <yoyo820701@gamil.com>
2023-08-07 16:45:29 +0800

    20230807

--- Bradjava/src/tw/org/iii/tutor/Brad21.java
+++ Bradjava/src/tw/org/iii/tutor/Brad21.java
@@ -5,7 +5,7 @@ import tw.org.iii.classes.Bike;
 public class Brad21 {
 
 	public static void main(String[] args) {
-		Bike b1 = new Bike();
+		Bike b1 = new Bike(null);
 		
 		System.out.println(b1.owner);
 		System.out.println(b1.getSpeed());
